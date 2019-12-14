b9f795074f925753fdf5e78ddd2fdfbed808e037
JW <jimmy.williams92@outlook.com>
2019-12-14 17:36:06 -0500

    adding class to keep track of clicks

--- familytree/treeapp.py
+++ familytree/treeapp.py
@@ -62,9 +62,96 @@ app.layout = html.Div(
     )   
 ])
 
-def retrieve_node_data(tree, node_idx):
-    return tree._graph.nodes[node_idx]
 
+def accumulate_clicks(new_click, click_list, max_click_num=2):
+    
+    enqueued = None
+    dequeued = None
+
+
+    if new_click in click_list:
+        dequeued = new_click
+        click_list.remove(new_click)
+
+    else:
+        if len(click_list) < max_click_num:
+            click_list.append(new_click)
+
+        else:
+            dequeued = click_list[-1]
+            click_list[:-1] = click_list[1:]
+            click_list[-1] = new_click
+    
+
+class clickList():
+    def __init__(self, max_clicks=2):
+        self.clicks = list()
+        self.max_clicks = max_clicks
+
+    def accumulate(self, new_click):
+        dequeued = None
+        
+        if new_click in self.clicks:
+            dequeued = new_click
+            self.clicks.remove(new_click)
+
+        else:
+            if len(self.clicks) < self.max_clicks:
+                self.clicks.append(new_click)
+
+            else:
+                dequeued = self.clicks[0]
+                self.clicks[:-1] = self.clicks[1:]
+                self.clicks[-1] = new_click
+    
+        return dequeued
+
+    def clear(self):
+        self.clicks = []
+
+
+def highlight_path(tree, node_list):
+
+    edge_tuples = list(zip(node_list[:-1], node_list[1:]))
+    
+    edge_indices = []
+    for idx, edge in enumerate(tree._graph.edges):
+        if edge in edge_tuples:
+            edge_indices.append(idx)
+
+    return edge_indices
+
+
+class markerUpdater():
+    def __init__(
+            self,
+            figure,
+            trace_ix,
+            on_color='red',
+            off_color='blue',
+            on_size='12',
+            off_size='10'
+    ):
+
+        self.figure = figure
+        self.trace_ix = trace_ix
+        self.on_color = on_color
+        self.off_color = off_color
+        self.on_size = on_size
+        self.off_size = off_size
+
+    def update_marker(self, marker_ix, direction='on'):
+        if direction == 'on':
+            self.figure['data'][self.trace_ix]['marker']['color'][marker_ix] = self.on_color
+            self.figure['data'][self.trace_ix]['marker']['size'][marker_ix] = self.on_size
+
+        else:
+            self.figure['data'][self.trace_ix]['marker']['color'][marker_ix] = self.off_color
+            self.figure['data'][self.trace_ix]['marker']['size'][marker_ix] = self.off_size
+
+
+clicks = clickList(max_clicks=2)
+updater = markerUpdater(figure=fig, trace_ix=1)
 
 @app.callback(
     [
@@ -77,10 +164,21 @@ def retrieve_node_data(tree, node_idx):
     [Input('example-graph', 'clickData')],
     [State('example-graph', 'figure')]
     )
+
 def display_click_data(clickData, fig):
     relationship = ""
     if clickData is not None:
         point_data = clickData['points'][0]['pointIndex']
+
+        removed_point_idx = clicks.accumulate(new_click=point_data)
+        
+        fig['data'][1]['marker']['color'][point_data] = 'red'
+        fig['data'][1]['marker']['size'][point_data] = 12
+
+        if removed_point_idx:
+            fig['data'][1]['marker']['color'][removed_point_idx] = 'blue'
+            fig['data'][1]['marker']['size'][removed_point_idx] = 10
+
 
         if len(selected_nodes) == 0:
             selected_nodes.append(point_data)
@@ -120,7 +218,7 @@ def display_click_data(clickData, fig):
     selected_nodes_str = [str(d) for d in selected_nodes]
     selected_nodes_str = '; '.join(selected_nodes_str)
 
-    return selected_nodes_str, fig, node_data[0], relationship, node_data[1]
+    return json.dumps(fig['data'][0]), fig, node_data[0], relationship, node_data[1]
     #return json.dumps(fig['data'][1])
 
 if __name__ == '__main__':
